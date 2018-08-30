e2276ad32d91d378232fce4e30056258fe5bd5b5
Amy Moretti <amyemoretti@gmail.com>
2018-08-29 21:34:24 -0500

    Minor update

--- liri.js
+++ liri.js
@@ -3,43 +3,44 @@ require("dotenv").config();
 var fs = require("fs");
 var keys = require("./keys.js");
 var request = require("request");
+var Spotify = require("node-spotify-api");
 
 switch (process.argv[2]) {
-    
+
     // concert-this
     case "concert-this":
-    Bands();
-    break;
+        Bands();
+        break;
 
     //spotify-this-song
     case "spotify-this-song":
-    //Spotify function
-    break;
+        //Spotify function
+        break;
 
     //movie-this
     case "movie-this":
-    Movie();
-    break;
+        Movie();
+        break;
 
     //do-what-it-says
     case "do-what-it-says":
-    // Do It function
-    break;
+        // Do It function
+        break;
 
     default: console.log("Invalid input");
 
 }
 
-// Concert Function
+// Concert Constructor
 function Bands() {
 
     var artist = process.argv[3];
 
-    this.searchConcert = function(artist) {
+    this.searchConcert = function (artist) {
         var url = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
 
 
-        request(url, function(err, response, body) {
+        request(url, function (err, response, body) {
             // Parse response into object
             var concertData = JSON.parse(body)[0];
             // console.log(concertData);
@@ -52,7 +53,7 @@ function Bands() {
 
 
             //Append data
-            fs.appendFile("random.txt", concertResponse, function(err) {
+            fs.appendFile("log.txt", concertResponse, function (err) {
                 if (err) throw err;
                 console.log(concertResponse);
             })
@@ -64,45 +65,56 @@ function Bands() {
 }
 
 
-// Spotify Function
+// Spotify Constructor
+function Songs() {
 
-// Movie function
+}
+
+
+
+// Movie Constructor
 function Movie() {
+
 
     var movieName = process.argv[3];
 
-    this.searchMovie = function(movieName) {
-        var url = "http://www.omdbapi.com/?i=tt3896198&apikey=aed0bcbf&t=" + movieName;
-        // console.log(url);
+    if (movieName === undefined) {
+        var movieName = "mr+nobody";
+    } 
+
+        this.searchMovie = function (movieName) {
+            var url = "http://www.omdbapi.com/?i=tt3896198&apikey=aed0bcbf&t=" + movieName;
+            // console.log(url);
 
 
-        request(url, function(err, response, body) {
-            // Parse response into object
-            var movieData = JSON.parse(body);
-            // console.log(movieData);
+            request(url, function (err, response, body) {
+                // Parse response into object
+                var movieData = JSON.parse(body);
+                // console.log(movieData);
 
-            var movieResponse = [
-                "Title: " + movieData.Title,
-                "Release Date: " + movieData.Released,
-                "IMDB Rating: " + movieData.Ratings[0].Value,
-                "Rotten Tomatoes Rating: " + movieData.Ratings[1].Value,
-                "Production Country: " + movieData.Country,
-                "Languge: " + movieData.Language,
-                "Plot: " + movieData.Plot,
-                "Stars: " + movieData.Actors,
-            ].join("\n");
+                var movieResponse = [
+                    "Title: " + movieData.Title,
+                    "Release Date: " + movieData.Released,
+                    "IMDB Rating: " + movieData.Ratings[0].Value,
+                    "Rotten Tomatoes Rating: " + movieData.Ratings[1].Value,
+                    "Production Country: " + movieData.Country,
+                    "Languge: " + movieData.Language,
+                    "Plot: " + movieData.Plot,
+                    "Stars: " + movieData.Actors,
+                ].join("\n");
 
 
-            //Append data
-            fs.appendFile("random.txt", movieResponse, function(err) {
-                if (err) throw err;
-                console.log(movieResponse);
+                //Append data
+                fs.appendFile("log.txt", movieResponse, function (err) {
+                    if (err) throw err;
+                    console.log(movieResponse);
+                })
+
             })
+        }
+        this.searchMovie(movieName);
 
-        })
     }
-    this.searchMovie(movieName);
 
-}
 
 // Do it function
